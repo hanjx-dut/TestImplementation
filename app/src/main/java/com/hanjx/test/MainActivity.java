73eed5dc655b5b94a1b14f03ef9f13e078f713b2
hanjx <hanjx@fenbi.com>
2020-09-02 15:25:22 +0800

    test dynamic dependencies

--- app/src/main/java/com/hanjx/test/MainActivity.java
+++ app/src/main/java/com/hanjx/test/MainActivity.java
@@ -3,6 +3,9 @@ package com.hanjx.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.hanjx.ui.SimpleToast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SimpleToast.toastShort(new TextView(this));
     }
 }
